898cc15666cfdb9bc4b0a79e3bef96cbeeef7883
Nicolas Charpentier <nicolas.charpentier079@gmail.com>
2018-06-12 11:52:21 -0400

    Get repository from GitHub event

--- index.js
+++ index.js
@@ -11,30 +11,22 @@ const CIRCLE_CI_ACTIVE_STATUS = [
 
 const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
 
-const circleciApiEndpoint = (url = '') => {
-  const { CIRCLE_TOKEN, ORG, REPO } = process.env;
+const circleciApiEndpoint = (repo, url = '') => {
+  const { CIRCLE_TOKEN } = process.env;
 
   if (!CIRCLE_TOKEN) {
     console.error('Environment variable CIRCLE_TOKEN is missing.');
   }
 
-  if (!ORG) {
-    console.error('Environment variable ORG is missing.');
-  }
-
-  if (!REPO) {
-    console.error('Environment variable REPO is missing.');
-  }
-
-  return `https://circleci.com/api/v1.1/project/github/${ORG}/${REPO}${url}?circle-token=${CIRCLE_TOKEN}`;
+  return `https://circleci.com/api/v1.1/project/github/${repo}${url}?circle-token=${CIRCLE_TOKEN}`;
 };
 
-const waitForCommitOnCircle = async (commit, tryCount = 0) => {
+const waitForCommitOnCircle = async (repo, commit, tryCount = 0) => {
   console.log(
     `Waiting for commit (${commit}) to show up on CircleCI.`,
     tryCount > 0 ? `(Retry ${tryCount})` : ''
   );
-  const builds = await checkIfCircleHaveCommit(commit);
+  const builds = await checkIfCircleHaveCommit(repo, commit);
 
   if (builds) {
     return builds;
@@ -45,11 +37,11 @@ const waitForCommitOnCircle = async (commit, tryCount = 0) => {
   }
 
   await sleep(10000);
-  return await waitForCommitOnCircle(commit, tryCount + 1);
+  return await waitForCommitOnCircle(repo, commit, tryCount + 1);
 };
 
-const checkIfCircleHaveCommit = async commit => {
-  const res = await fetch(circleciApiEndpoint());
+const checkIfCircleHaveCommit = async (repo, commit) => {
+  const res = await fetch(circleciApiEndpoint(repo));
   const builds = await res.json();
 
   const isHavingCommit = builds.some(
@@ -62,12 +54,13 @@ const checkIfCircleHaveCommit = async commit => {
 const server = micro(async (req, res) => {
   const payload = await micro.json(req);
   const headCommitSha = payload.head_commit.id;
+  const repo = payload.repository.full_name;
 
   console.log(
     `Push event received with the following head commit SHA: ${headCommitSha}`
   );
 
-  const builds = await waitForCommitOnCircle(headCommitSha);
+  const builds = await waitForCommitOnCircle(repo, headCommitSha);
 
   if (!builds) {
     console.log('The commit has not showed up on CircleCi.');
@@ -118,7 +111,7 @@ const server = micro(async (req, res) => {
 
       buildsToCancel.map(async ({ build_num }) => {
         console.log(`Build #${build_num} will be canceled.`);
-        return await fetch(circleciApiEndpoint(`/${build_num}/cancel`), {
+        return await fetch(circleciApiEndpoint(repo, `/${build_num}/cancel`), {
           method: 'POST',
         });
       });
